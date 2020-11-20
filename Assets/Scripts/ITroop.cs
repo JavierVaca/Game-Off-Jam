7795230049bcc6bef56753d7897d67132db58054
Javier <javiervaca96@gmail.com>
2020-11-19 23:51:56 -0600

    scripts addded

---
+++ Assets/Scripts/ITroop.cs
@@ -0,0 +1,13 @@
+interface ITroop 
+{
+    int Health {get; set;}
+    int Damage { get; set; }
+    float ViewRange { get; set; }
+    float AttackRange { get; set; }
+    float Speed { get; set; }
+
+    void Move();
+    void Attack();
+    bool ChangeHealth(int i);
+    bool Die();
+}
---
+++ Assets/Scripts/TroopSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TroopSelector : MonoBehaviour
+{
+    List<ITroop> selectedTroops;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
